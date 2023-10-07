bb0fbf44c619467d3e46e523011402fde6f676f7
rahul-9340 <142489789+rahul-9340@users.noreply.github.com>
2023-10-07 11:09:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,29 +1,37 @@
 function convertToRoman(num) {
-  	const obj = {
-      0:['M',1000], 
-      1:['D', 500], 
-      2:['C', 100], 
-      3:['L', 50], 
-      4:['X', 10], 
-      5:['V', 5], 
-      6:['I', 1]
-    };
+  const obj = {
+    0: ['M', 1000],
+    1: ['CM', 900],
+    2: ['D', 500],
+    3: ['CD', 400],
+    4: ['C', 100],
+    5: ['XC', 90],
+    6: ['L', 50],
+    7: ['XL', 40],
+    8: ['X', 10],
+    9: ['IX', 9],
+    10: ['V', 5],
+    11: ['IV', 4],
+    12: ['I', 1]
+  };
 
-let result = "";
+  if (num < 1 || num > 100000) {
+    return "Invalid input. Please enter a number between 1 and 100,000.";
+  }
 
-	for(let i=0; i<=6; i++)
-	{
-while(num>=obj[i][1]) {
-	result = result+obj[i][0];
-	num = num-obj[i][1];
-     }
-		
+  let result = "";
+
+  for (let key = 0; key <= 12; key++) {
+    while (num >= obj[key][1]) {
+      result += obj[key][0];
+      num -= obj[key][1];
     }
-	return result;
-}
-// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
+  }
 
-console.log(convertToRoman(36));
+  return result;
+}
+
+console.log(convertToRoman(799)); // Output: "DCCXCIX"
 
 
 
